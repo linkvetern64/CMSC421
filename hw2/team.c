68cbb92c4471eea657b6ea9f63ff3678abc6053e
Joshua Standiford <jstand1@umbc.edu>
2016-09-26 02:35:58 -0400

    Working menu

--- hw2/team.c
+++ hw2/team.c
@@ -13,35 +13,73 @@ int main(int argc, char *argv[]){
   if(argc != 5){
     return 0;
   }
-  //int input;
-  //int selection = 0;
+
+  int input;
+  int selection = 9;
 
   int pid;
   int PID[4];
-  //int fd[2];
-  //pipe(fd);
+  int fd[2];
+  int pipeRet = pipe(fd);
+  if(pipeRet == 0){
+    printf("Successful pipe\n");
+  }
   //Creates forked processes
   
   for(int i = 0; i <= 3; i++){
     switch(pid = fork()){
     case 0:
-      printf("Child process\n");
-      //childProcess();
-      //child process
+      //Close READ
+      close(fd[0]);
+      //Close WRITE
+      //close(fd[1]);
+      //child case
       return 0;
    
     case -1:
       //Error - do nothing
-      printf("Error in forking\n");
       break;
+
     default:
       PID[i] = pid;
-      //parent process
+      close(fd[1]);
       //parentProcess();
       break;
     }
   }
-    
+
+  if(getpid() > 0){
+    while(selection > -1){
+	printf("Main Menu:\n");
+	printf("1. Throw ball directory to a player\n");
+	printf("2. Field ball\n");
+	printf("3. Show player statistics\n");
+	printf("4. End game\n");
+
+	selection = scanf("%d", &input);
+
+	switch(input){
+	case 1:
+	  printf("Throwing ball...\n");
+	  break;
+	case 2:
+	  printf("Fielding ball...\n");
+	  break;
+	case 3:
+	  printf("Showing player stats...\n");
+	  break;
+	case 4:
+	  printf("Ending game...\n");
+	  return 0;
+	  break;
+	default:
+	  printf("Value of input is %d\n", input);
+	  break;
+	}	
+      }
+  }
+  
+  //Test kills the PID's -  DELETE THIS
   for(int j = 0; j < 4; j++){
     printf("PID for child %d = %d\n", j, PID[j]);
     if(kill(PID[j]) == 0){
@@ -51,27 +89,6 @@ int main(int argc, char *argv[]){
 	printf("Failed to kill PID\n");
       }
   }
-  
-  /*
-  //Menu
-  while(true){
-  printf("Main Menu:\n");
-  printf("1. Throw ball directory to a player\n");
-  printf("2. Field ball\n");
-  printf("3. Show player statistics\n");
-  printf("4. End game\n");
-  selection = scanf("%d", input);
-  }
-  //End menu
-  
-  FILE *file = fopen("/dev/urandom", "r");
-  int rand = fscanf(file, "%[^n]");
-  printf("Num - %d\n", rand);
-  for(int i = 1; i < argc; i++){
-    printf("Name %d - %s\n",i, argv[i]);
-  }
-  fclose(file);
-  */
   return 0;
 }
 
