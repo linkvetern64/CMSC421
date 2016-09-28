de0e7f7e66270b50cd90bbcc12f1f9c101d8ffdc
Joshua Standiford <jstand1@umbc.edu>
2016-09-28 01:00:19 -0400

    Added working signal and handlers

--- hw2/team.c
+++ hw2/team.c
@@ -6,22 +6,24 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
-void childProcess(void); //Prototype of child process
-void parentProcess(void); //Prototype of parent process
 
 int main(int argc, char *argv[]){
+  //Exits program if too many or too little arguments
   if(argc != 5){
     printf("Error too many or too little arguments.  Need exactly 4\n");
     return 0;
   }
+
   char * players[4];
   char * positions[4];
-  
+  char a;
+  int fp;
+  int fdT;
   positions[0] = "1B";
   positions[1] = "2B";
   positions[2] = "SS";
   positions[3] = "3B";
-  int input;
+  int input, playerTo;
   int pid;
   int selection = 9;
   int PID[4];
@@ -35,9 +37,13 @@ int main(int argc, char *argv[]){
   //Creates forked processes
   for(int i = 0; i <= 3; i++){
     switch(pid = fork()){
+    //Child process
     case 0:
       //Close Read
-      dup2(fd[i * 2], 0);
+      if(dup2(fd[i * 2], STDIN_FILENO) == -1){
+	printf("dup2 error\n");
+      }
+      
       //printf("Testing %d, %d\n", (i * 2), (1 + i * 2)); 
       close(fd[i * 2]);
       close(fd[1 + i * 2]);
@@ -65,53 +71,95 @@ int main(int argc, char *argv[]){
 	printf("2. Field ball\n");
 	printf("3. Show player statistics\n");
 	printf("4. End game\n");
-	printf("5. Print child PIDS\n");
-	printf("6. Kill PIDS\n");
 	selection = scanf("%d", &input);
 
 	switch(input){
 	case 1:
-	  printf("Throwing ball...\n");
+	  printf("Throwing ball to who?\n");
+	  for(int i = 1; i < 5; i++){
+	    printf("%d %s\n", i, argv[i]);
+	  }
+	  selection = scanf("%d", &playerTo);
+	  switch(playerTo){
+	  case 1:
+	    kill(PID[0], SIGUSR1);
+	    break;
+
+	  case 2:
+	    kill(PID[1], SIGUSR1);
+	    break;
+
+	  case 3:
+	    kill(PID[2], SIGUSR1);
+	    break;
+
+	  case 4:
+	    kill(PID[3], SIGUSR1);
+	    break;
+     
+	  default:
+	    printf("Illegal entry, returning to main menu\n");
+	    break;
+	  }
 	  break;
+	  
 	case 2:
 	  printf("Fielding ball...\n");
 	  break;
+	  
 	case 3:
 	  printf("Showing player stats...\n");
 	  break;
+	  
 	case 4:
 	  printf("Ending game...\n");
+	  for(int i = 0; i < 4; i++){
+	    kill(PID[i], SIGQUIT);
+	  }
 	  waitpid();
 	  return 0;
 	  break;
-	//Test case
-	case 5:
-	  for(int l = 0; l < 4; l++){
-	    printf("Child ID = %d \n", PID[l]);
-	  }
-	  break;
-	//Test case
-	case 6:
-	  //Test kills the PID's -  DELETE THIS
-	  for(int j = 0; j < 4; j++){
-	    printf("PID for child %d = %d\n", j, PID[j]);
-	    if(kill(PID[j], 0) == 0){
-	      kill(PID[j], SIGKILL);
-	      printf("Error:(%s)\n", strerror(errno));
-	      //printf("Succeeded in killing PID\n");
-	    }
-	    else{
-	      printf("Failed to kill PID\n");
-	    }
-	  }
-	  break;
-	case 7:
 	  
-	  write(fd[0], "Test", strlen("Test") + 1);
-	  printf("Error:(%s)\n", strerror(errno));
+	case 5:
+	  //Test FD is open / closed
+	  /*if(fcntl(fd, F_GETFD) == -1){
+	    printf("Failure\n");
+	  }
+	  else{
+	    printf("Success\n");
+	  }*/
+	  if(close(fd[2])){
+	    perror("Close");
+	  }
+	  else{
+	    printf("All good\n");
+	  }
+	  break;
+
+	case 6:
+	  fp = open("/dev/urandom", 00);
+	  if(fp < 0){
+	    fprintf(stderr, "Error opening\n");
+	  }
+	  else{
+	    printf("Opened FD %d\n",fp);
+	  }
+
+	  char buf[1];
+	  
+	  size_t amount_to_read = sizeof(buf); 
+	  ssize_t amount_read = read(fp, buf, amount_to_read); 
+	  if(amount_read < 0) { 
+	    fprintf(stderr,"Error reading\n"); 
+	  } 
+	  else
+	    { 
+	      printf("Read %zd bytes\n", amount_read);
+	      printf("%d\n", ((int)buf)%4);
+	    }
+	  close(fp);
 	  break;
 	default:
-	  
 	  printf("Value of input is %d\n", input);
 	  break;
 	}	
@@ -120,14 +168,4 @@ int main(int argc, char *argv[]){
  
   
   return 0;
-}
-
-void childProcess(void){
-  printf("Child Process\n");
-  //close(fd[1]);
-}
-
-void parentProcess(void){
-  printf("Parent Process\n");
-  //close(fd[0]);
 }
